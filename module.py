b18b546ab5c1dfa0b296abbaffe1960e1f427104
Sarah Bunker <ChemEnCoder@gmail.com>
2023-05-01 18:16:28 -0600

    adding info about pip, module mananger for python

--- module.py
+++ module.py
@@ -1,3 +1,25 @@
 import new
 
 new.say_hi()
+
+'''
+modules can be on your local system or you can get them from online
+
+online modules stored on pypi https://pypi.org/project/numpy/
+
+example
+```
+pip install numpy
+```
+pip used to instal numpy
+
+
+I needed to install pip
+```
+sudo apt install python3-pip
+```
+'''
+
+import numpy as np
+a = np.array([1, 2, 3, 4, 5, 6])
+print(a)
